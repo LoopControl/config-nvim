1c6903ff924f57c6b04a5f2daf1b55dcdcd351a9
DevZero <contact@devzero.co>
2024-12-06 18:34:52 -0500

    dashboard

--- lua/plugins/mein.lua
+++ lua/plugins/mein.lua
@@ -13,7 +13,23 @@ return {
       quickfile = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
-      dashboard = { enabled = true },
+      dashboard = {
+        enabled = true,
+        sections = {
+          { section = "header" },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+--          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
+        },
+        preset = {
+          -- Used by the `header` section
+          header = [[
+░▀▀█░█▀▀░█▀▀░█░█░▀█▀░█▄█
+░░░█░█▀▀░█▀▀░▀▄▀░░█░░█░█
+░▀▀░░▀▀▀░▀▀▀░░▀░░▀▀▀░▀░▀]]
+        },
+      },
     },
     keys = {
       { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
